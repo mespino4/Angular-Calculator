25eb8690ecd171617822faec6a71a9081093c3b9
Mely Espino <90160548+melyespinofl96@users.noreply.github.com>
2023-05-24 20:24:09 -0500

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,41 +9,38 @@ export class AppComponent {
   title = 'Angular-Calculator';
 
   displayNumber: string = '0';
-  hidderNumber: string = '0';
   isFuncClick: boolean = false;
-  isNumberClick: boolean = false
 
   resultDisplay: string = '';
   equalReset: boolean = false;
 
-  onClickValue (val: string, type: any){ //when you press any button
+  /*This method is called whenever any button is clicked, it receives the value as a string and
+  the type of button that was pressed(wether number or operator)*/
+  onClickValue (val: string, type: any){
     if(type == 'number'){
       this.onNumberClick(val);
     }
     else if(type == 'function'){
-
       this.onFunctionClick(val)
     }
 
-    if(!this.isFuncClick)                           //if a funciton was not clicked
-      this.resultDisplay = eval(this.displayNumber);//evaluate the number that was displayed
+    if(!this.isFuncClick)                                                   //if a funciton was not clicked
+      this.resultDisplay = eval(this.displayNumber);                        //evaluate the string on screen
   }
 
+  /*This method is called whenver a number button is clicked*/
   onNumberClick(val: string){
-    this.isNumberClick = true;
-    if(this.displayNumber == '0' || this.equalReset){ //if the number is 0
-      this.displayNumber = val;                       //add number to display string
+    if(this.displayNumber == '0' || this.equalReset){                       //if the number is 0
+      this.displayNumber = val;                                             //add number to display string
       this.equalReset = false;
-    }
-    else{
-      this.displayNumber += val;                      //add the number to the display string
-    }
-
+    }else
+      this.displayNumber += val;                                            //add the number to the display string
+    
     this.isFuncClick = false;
   }
 
+  /*This method is called whenver a function type button is clicked*/
   onFunctionClick(val: string){
-    this.isNumberClick = false;
     this.isFuncClick = true;
     this.equalReset = false;
 
@@ -58,42 +55,33 @@ export class AppComponent {
       null
     else
       this.displayNumber += val;
-      
-    /*
-    if(val == '=' || val == 'ac')
-      null
-    else
-      this.displayNumber += val;
-    */
-    
-  }
-  
-  multiplyButton(){
   }
 
-  divisionButton(){}
-  additionButton(){}
-  subtractButton(){}
+  /*This method is called whenver a function type button is clicked*/
   backspaceButton(){}
 
-  onEqualPress(){ //if equal button is pressed
+  /*This method is called whenver the "=" button is pressed*/
+  onEqualPress(){
     this.equalReset = true;
 
     this.displayNumber = this.resultDisplay.toString();
     this.resultDisplay = '';
   }
 
+  /*This method is called whenver the "AC" button is pressed*/
   clearAll(){
     this.resultDisplay = '';
     this.displayNumber = '0';
   }
 
+  /*This method checks the last character in the string that is given to oit is a number or not*/
   isLastCharNumber(str: string): boolean {
     const lastChar = str.charAt(str.length - 1);
     return !isNaN(Number(lastChar));
   }
-
   
+  /*This method checks if the string given to it has a function type value as its last character.
+  This methord is used to avoid the user adding 2 operator buttons on the display screen ex. 1++1*/
   isFunctionValid(str: string): boolean {
     const lastChar = str.charAt(str.length - 1);
     if(lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/' ||
@@ -102,5 +90,4 @@ export class AppComponent {
     else
       return true
   }
-  
 }
